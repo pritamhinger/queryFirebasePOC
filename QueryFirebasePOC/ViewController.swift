19a0d4c255ab6f272394c8392d6cf18b5d3d7af0
Pritam Hinger <pritam@mitron.tv>
2020-10-17 00:31:57 +0530

    Initial Commit

---
+++ QueryFirebasePOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QueryFirebasePOC
+//
+//  Created by Pritam Hinger on 17/10/20.
+//  Copyright © 2020 Pritam Hinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
